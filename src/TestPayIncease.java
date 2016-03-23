bd1619fd9830565dc5232d8b7c7c07f43a6f4dce
kat <kate.lebid@gmail.com>
2016-03-23 23:52:39 +0200

    test

--- src/TestPayIncease.java
+++ src/TestPayIncease.java
@@ -9,7 +9,7 @@ public class TestPayIncease {
 	public static void main(String[] args) {
 
         Person workers[] = new Person[3];
-		workers[0] = new Employee("John");
+		workers[0] = new Employee("John2");
 		workers[1] = new Contractor("Mary");
 		workers[2] = new Employee("Steve");
 		
